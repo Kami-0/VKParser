8fcab75549008af3c89f86913e71b69719399c2c
Makarov <makarovdaniil2000@mail.ru>
2020-09-14 00:07:30 +0700

    переопределен метод getPostByDateRange

--- biz/biz-api/src/main/java/ru/pet/kami/biz/api/repository/VkRepositoryApi.kt
+++ biz/biz-api/src/main/java/ru/pet/kami/biz/api/repository/VkRepositoryApi.kt
@@ -1,9 +1,9 @@
 package ru.pet.kami.biz.api.repository
 
 import com.vk.api.sdk.objects.wall.WallPostFull
-import java.time.LocalDate
+import java.time.Instant
 
 interface VkRepositoryApi {
     //Поиск включительный
-    fun getPostByDateRange(ownerId: Int, startDate: LocalDate, finishDate: LocalDate): List<WallPostFull>
+    fun getPostByDateRange(ownerId: Int, startDate: Instant, finishDate: Instant): List<WallPostFull>
 }
--- biz/biz-core/src/main/java/ru/pet/kami/biz/core/repository/VkRepository.kt
+++ biz/biz-core/src/main/java/ru/pet/kami/biz/core/repository/VkRepository.kt
@@ -1,16 +1,42 @@
 package ru.pet.kami.biz.core.repository
 
 import com.vk.api.sdk.objects.wall.WallPostFull
+import com.vk.api.sdk.objects.wall.responses.GetResponse
 import org.springframework.stereotype.Repository
 import ru.pet.kami.biz.api.repository.VkRepositoryApi
 import ru.pet.kami.sdk.VkSdkApi
-import java.time.LocalDate
+import java.time.Instant
 
 @Repository
 class VkRepository(
-    val vkSdkApi: VkSdkApi
+    private val vkSdkApi: VkSdkApi
 ) : VkRepositoryApi {
-    override fun getPostByDateRange(ownerId: Int, startDate: LocalDate, finishDate: LocalDate): List<WallPostFull> {
-        vkSdkApi.getPostsFromGroupWall(ownerId)
+    override fun getPostByDateRange(ownerId: Int, startDate: Instant, finishDate: Instant): List<WallPostFull> {
+        val result: MutableList<WallPostFull> = mutableListOf()
+        val startDateUT = startDate.epochSecond
+        val finishDateUT = finishDate.epochSecond
+
+        do {
+            val posts = vkSdkApi.getPostsFromGroupWall(ownerId)
+            val lastDate = posts?.items?.last()?.date!!
+            when {
+                lastDate < startDateUT -> continue
+                lastDate in startDateUT..finishDateUT -> {
+                    addValidValues(posts, startDateUT, finishDateUT, result)
+                    break
+                }
+                lastDate >= startDateUT -> addValidValues(posts, startDateUT, finishDateUT, result)
+            }
+        } while (true)
+
+        return result
+    }
+
+    private fun addValidValues(posts: GetResponse, startDateUT: Long, finishDateUT: Long, result: MutableList<WallPostFull>) {
+        posts.items?.forEach {
+            if (it.date in startDateUT..finishDateUT) {
+                result.add(it)
+            }
+        }
     }
 }
